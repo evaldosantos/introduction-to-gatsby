8df82957c7dc8dbae6ffb3491e7e332a2b50f177
Evaldo Santos <jesantosjr@protonmail.com>
2021-02-28 13:25:17 -0300

    link pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby";
 
 const About = () => (
   <>
     <h1>About Me</h1>
     <p>This is my personal website.</p>
+    <Link to="/">&larr; back to home</Link>
   </>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby";
 
 export default function Home() {
-  return <h1>Hello Gatsby!</h1>
+  return (
+    <>
+      <h1>Hello Gatsby!</h1>
+      <Link to="/about">Learn about me &rarr;</Link>
+    </>
+  )
 }
